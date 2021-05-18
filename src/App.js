620485b44193eca0aa03994b95ed655741c97843
sonam-shrish <sonamshrish1618@gmail.com>
2021-05-18 15:02:20 +0545

    post component using useState, mapping, props and installed material UI. working on posts

--- src/App.js
+++ src/App.js
@@ -1,6 +1,32 @@
 import './App.css';
+import Post from './components/Post';
+import { useState } from 'react';
 
 function App() {
+	const [posts, setPosts] = useState([
+		{
+			username: 'Sonam Shrish Magar',
+			caption:
+				"You've gone thru the finest school alrigh miss lonely but u know u only used to get juiced in it",
+			imgUrl:
+				'https://scontent.fktm8-1.fna.fbcdn.net/v/t1.6435-9/128066231_101825875120995_2278377534081368595_n.jpg?_nc_cat=110&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=5IyAwFgkW7YAX8REgGo&_nc_ht=scontent.fktm8-1.fna&oh=54bc15bfba7199dfccb491cca63e4c6f&oe=60C96976',
+		},
+		{
+			_id: 1,
+			username: 'Nirzal Mahat',
+			caption: 'I am a django developer.May DJESUS CHRIST! bless yall',
+			imgUrl:
+				'https://scontent.fktm8-1.fna.fbcdn.net/v/t1.6435-9/131981824_1958814704260075_5497502334936332706_n.jpg?_nc_cat=110&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=y8ACx3a-_wUAX8ex2bI&_nc_ht=scontent.fktm8-1.fna&oh=44e80d3aa31629e83fdf6c0d8591bab7&oe=60C83201',
+		},
+		{
+			_id: 2,
+			username: 'Mick Jaggaer',
+			caption: 'Graceless lady, u know who I am',
+			imgUrl:
+				'https://upload.wikimedia.org/wikipedia/commons/1/10/The_Rolling_Stones_Summerfest_in_Milwaukee_-_2015.jpg',
+		},
+	]);
+
 	return (
 		<div className='App'>
 			<div className='app__header'>
@@ -10,7 +36,14 @@ function App() {
 					className='app__headerImg'
 				/>
 			</div>
-			Let's go. Let's create an instagram clone Hey
+			{posts.map((post) => (
+				<Post
+					username={post.username}
+					caption={post.caption}
+					imageUrl={post.imgUrl}
+				/>
+			))}
+			Hello
 		</div>
 	);
 }
---
+++ src/components/Post.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Post.css';
+import { Avatar } from '@material-ui/core';
+
+const Post = ({ username, imageUrl, caption }) => {
+	return (
+		<div className='post'>
+			{/* post header: avatar, username */}
+			<div className='post__header'>
+				<Avatar
+					className='post__avatar'
+					alt='SonamShrish'
+					src='./static/images'
+				/>
+				<h3>{username}</h3>
+			</div>
+
+			{/* image */}
+			<img className='post__img' src={imageUrl} alt='' />
+			{/* caption */}
+			<h4 className='post__text'>
+				<strong>{username}</strong>: {caption}
+			</h4>
+		</div>
+	);
+};
+
+export default Post;
