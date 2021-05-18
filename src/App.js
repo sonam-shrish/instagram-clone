9f1d9b24d4461be26353ec1c910dbc8ad989a0a2
sonam-shrish <sonamshrish1618@gmail.com>
2021-05-18 13:10:37 +0545

    set up firebase, header

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+	return (
+		<div className='App'>
+			<div className='app__header'>
+				<img
+					src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/320px-Instagram_logo.svg.png'
+					alt='header image'
+					className='app__headerImg'
+				/>
+			</div>
+			Let's go. Let's create an instagram clone Hey
+		</div>
+	);
+}
+
+export default App;
